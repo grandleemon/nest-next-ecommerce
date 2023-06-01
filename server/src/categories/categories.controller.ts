3a77ed5580ea7cfa85622bf43136f578050763c0
Max <zeetreex@gmail.com>
2023-06-01 16:46:21 +0600

    created put, delete, create endpoints

--- server/src/categories/categories.controller.ts
+++ server/src/categories/categories.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post, Put
+} from '@nestjs/common';
 import { CategoriesService } from "./categories.service";
+import { CreateCategoryDto } from "./dto/create-category.dto";
 
 @Controller('categories')
 export class CategoriesController {
@@ -8,5 +16,20 @@ export class CategoriesController {
   @Get()
   async getAll() {
     return await this.categoriesService.getAll();
+  }
+
+  @Post()
+  async create(@Body() dto: CreateCategoryDto) {
+    return await this.categoriesService.create(dto);
+  }
+
+  @Put(":id")
+  async update(@Param("id") id: number, @Body() dto: CreateCategoryDto) {
+    return await this.categoriesService.update(id, dto);
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: number) {
+    return await this.categoriesService.delete(id);
   }
 }
--- server/src/categories/categories.service.ts
+++ server/src/categories/categories.service.ts
@@ -2,12 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Category } from "./category.model";
 import { Repository } from "typeorm";
+import { CreateCategoryDto } from "./dto/create-category.dto";
 
 @Injectable()
 export class CategoriesService {
   constructor(@InjectRepository(Category) private categoryRepository: Repository<Category>) {}
 
-  getAll(): Promise<Category[]> {
+  getAll() {
     return this.categoryRepository.find();
+  }
+
+  create(dto: CreateCategoryDto) {
+    const category = new Category();
+
+    category.name = dto.name;
+    category.slug = dto.name.toLowerCase().split(" ").join("-");
+
+    return this.categoryRepository.save(category);
+  }
+
+  async update(id: number, dto: CreateCategoryDto) {
+    const category = await this.categoryRepository.findOne({where: {id}})
+
+    if(!category) return `Category with id: ${id} doesn't exist.`
+
+    category.name = dto.name;
+    category.slug = dto.name.toLowerCase().split(" ").join("-");
+
+    return this.categoryRepository.save(category);
+  }
+
+  delete(id: number) {
+    return this.categoryRepository.delete(id)
   }
 }
